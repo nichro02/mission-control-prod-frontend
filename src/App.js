2bdce0ef3f7e11ac1b6a478ae2ec26a885f29cbc
nichro02 <nichro02@gmail.com>
2021-04-08 19:16:05 -0600

    Created Board, Column, Login, Profile, Signup, and Task component skeletons. Added and tested switches for Board and Profile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+//import switch and route to navigate between pages
 import { Switch, Route } from 'react-router-dom'
+
+//import components
 import Home from './components/Home'
 import Layout from './components/Layout'
+import Board from './components/Board'
+import Profile from './components/Profile'
 
 const App = () => {
   return (
     <Layout>
       <Switch>
         <Route exact path={['/','/home']} component={Home}/>
+        <Route exact path={'/board/:id'} component={Board}/>
+        <Route exact path={'/profile/:id'} component={Profile}/>
       </Switch>
     </Layout>
   
---
+++ src/components/Profile.js
@@ -0,0 +1,10 @@
+
+
+const Profile = () => {
+    
+    return(
+        <div>Profile goes here</div>
+    )
+}
+
+export default Profile
