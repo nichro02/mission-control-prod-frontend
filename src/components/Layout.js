f6f2be1e2ea4384372ad380c7f1b3fa28f4777c4
nichro02 <nichro02@gmail.com>
2021-04-16 16:58:58 -0600

    added functionality to navbar so users can navigate website

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,68 @@
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { getCurrentUser, logout } from '../services/auth.service'
+
 import '../css/layout.css'
 
 const Layout = (props) => {
+    const [currentUser, setCurrentUser] = useState(undefined)
+
+    useEffect(() => {
+        // grab getCurrentuser from the auth service
+        const user =  getCurrentUser();
+    
+        if (user) {
+          // Set current user to the currentUser state
+          setCurrentUser(user);
+        }
+    }, [])
+
+    const logOut = () => {
+        logout()
+    }
 
     return(
         <div>
-            <nav id='navbar'>
-                <h1>Mission Control</h1>
+            <nav id='navbar' class='navGrid'>
+                <Link to={'/home'} class='inline'>
+                    <h1>Mission Control</h1>
+                </Link>
+                <div>
+                
+                {currentUser ? (
+                    <div class='navGrid'>
+                        <Link to={'/home'} class='inline'>
+                            <h3>Home</h3>
+                        </Link>
+                    <li>
+                        <a href='/login' onClick={logOut}>
+                            Logout
+                        </a>
+                    </li>
+                    </div>
+                ): (
+                    <div class='navGrid'>
+                        <li>
+                            <Link to={'/home'} class='inline'>
+                            <h3>Home</h3>
+                            </Link>
+                        </li>
+                        
+                    <li>
+                        <Link to={'/login'}>
+                            <h3>Login</h3>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'/signup'}>
+                            <h3>Signup</h3>
+                        </Link>
+                    </li>
+                    </div>
+                )}
+                </div>
+                
+
             </nav>
             <div id='container'>{props.children}</div>
         </div>
